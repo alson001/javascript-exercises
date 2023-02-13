eefd5b91404c09b48d828645fbefb5c288a504ec
alson001 <alsonang01@gmail.com>
2023-02-13 19:40:46 +0800

    Did some javascript exercises

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    len = string.length - 1
+    arr = []
+    for (let i = len; i >= 0 ; i--) {
+        arr[i] = string.charAt(len - i)
+    }
+    return arr.toString().replaceAll(",", "")
 };
 
 // Do not edit below this line
